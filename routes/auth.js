b951608c0f5c84a34b5000b6a6435a6fdcf88971
Sahil Ahmed <ahmedsahil5498@gmail.com>
2025-08-19 09:40:57 +0530

    bug fixed

--- routes/auth.js
+++ routes/auth.js
@@ -42,7 +42,7 @@ authRouter.post("/auth/signUp",async(req,res)=>{
         res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    // sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
@@ -85,7 +85,7 @@ authRouter.post("/auth/signIn",async(req,res)=>{
           res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    // sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
